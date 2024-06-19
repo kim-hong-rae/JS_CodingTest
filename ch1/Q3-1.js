59ecb19b2d931e3f2fcb7dc5be8e7cd0737c43d0
kimhongrae <khr0690@hanmail.net>
2024-06-19 20:51:02 +0900

    feat: 이중 포인터

---
+++ ch1/Q3-1.js
@@ -0,0 +1,25 @@
+//문제
+//write a function called sumZero which accepts a sorted array of integers. The function should find the first pair where the sum is 0. Return an array that includes both values that sum to zero or undefined if a pari does not exist
+
+function sumZero(arr) {
+  for (let i = 0; i < arr.length; i++) {
+    for (let j = i + 1; j < arr.length; j++) {
+      if (arr[i] + arr[j] === 0) {
+        return [arr[i], arr[j]];
+      }
+    }
+  }
+}
+
+function sumZero(arr) {
+  let left = 0;
+  let right = arr.length - 1;
+  let sum = arr[left] + arr[right];
+  while (left < right) {
+    if (sum === 0) {
+      return [arr[left], arr[right]];
+    } else if (sum > 0) {
+      right--;
+    } else left++;
+  }
+}
