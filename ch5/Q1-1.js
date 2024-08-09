9337dadd6675ba53df3d198f41d0c4289b680118
kimhongrae <khr0690@hanmail.net>
2024-08-09 15:20:55 +0900

    feat : BST 구현

---
+++ ch5/Q1-1.js
@@ -0,0 +1,63 @@
+class Node {
+  constructor(value) {
+    this.value = value;
+    this.left = null;
+    this.right = null;
+  }
+}
+
+class BinarySearchTree {
+  constructor() {
+    this.root = null;
+  }
+  insert(value) {
+    let newNode = new Node(value);
+    if (this.root === null) {
+      this.root = newNode;
+      return this;
+    }
+    let current = this.root;
+    while (true) {
+      if (current < newNode) {
+        if (current.left === null) {
+          current.left = newNode;
+          return this;
+        } else {
+          current = current.left;
+        }
+      } else {
+        if (current.right === null) {
+          current.right = newNode;
+          return this;
+        } else {
+          current = current.right;
+        }
+      }
+    }
+  }
+  contains(value) {
+    if (this.root === null) return false;
+    let current = this.root;
+    let found = false;
+    while (current & !found) {
+      if (value < current.value) {
+        current = current.left;
+      } else if (value > current.value) {
+        current = current.right;
+      } else {
+        return true;
+      }
+    }
+    return false;
+  }
+}
+
+let tree = new BinarySearchTree();
+tree.root = new Node(10);
+tree.root.left = new Node(7);
+tree.root.right = new Node(15);
+tree.root.left.right = new Node(9);
+
+//일반적으로
+//insertion - O(log n)
+//searching - O(long n)
