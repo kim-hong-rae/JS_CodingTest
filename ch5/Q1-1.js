dbd539841ee1dd287a2a60723afbb744ec9354e8
kimhongrae <khr0690@hanmail.net>
2024-08-30 16:25:18 +0900

    feat : 트리 순회

--- ch5/Q1-1.js
+++ ch5/Q1-1.js
@@ -50,14 +50,63 @@ class BinarySearchTree {
     }
     return false;
   }
+  BFS() {
+    let queue = [];
+    let data = [];
+    node = this.root;
+    queue.push(node);
+    while (queue.length) {
+      node = queue.shift();
+      data.push(node);
+      if (node.left) queue.push(node.left);
+      if (node.right) queue.push(node.right);
+    }
+    return data;
+  }
+  DFSPreOrder() {
+    let data = [];
+    function traverse(node) {
+      data.push(node.value);
+      if (node.left) traverse(node.left);
+      if (node.right) traverse(node.right);
+    }
+    traverse(this.root);
+    return data;
+  }
+
+  DFSPostOrder() {
+    let data = [];
+    function traverse(node) {
+      if (node.left) traverse(node.left);
+      if (node.right) traverse(node.right);
+      data.push(node.value);
+    }
+    traverse(this.root);
+    return data;
+  }
+  DFSInOrder() {
+    let data = [];
+    function traverse(node) {
+      if (node.left) traverse(node.left);
+      data.push(node.value);
+      if (node.right) traverse(node.right);
+    }
+    traverse(this.root);
+    return data;
+  }
 }
 
 let tree = new BinarySearchTree();
-tree.root = new Node(10);
-tree.root.left = new Node(7);
-tree.root.right = new Node(15);
-tree.root.left.right = new Node(9);
+tree.insert(10);
+tree.insert(6);
+tree.insert(15);
+tree.insert(3);
+tree.insert(8);
+tree.insert(20);
 
+console.log(tree.DFSPreOrder());
+console.log(tree.DFSPostOrder());
+console.log(tree.DFSInOrder());
 //일반적으로
 //insertion - O(log n)
 //searching - O(long n)
